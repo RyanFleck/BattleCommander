07ced4e6c61d013198c1283313f2113dfd7a5a73
ryanfleck <ryan.fleck@protonmail.com>
2017-06-05 16:21:54 -0400

    Automatic bash-script commit.

--- core/src/com/libgdx/battlecommander/BattleCommander.java
+++ core/src/com/libgdx/battlecommander/BattleCommander.java
@@ -1,156 +1,154 @@
 package com.libgdx.battlecommander;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 /*
- * Panning battle map:
+ * Resources used:
  * https://github.com/libgdx/libgdx/wiki/Orthographic-camera
- * 
- * 
+ * http://www.gamefromscratch.com/post/2014/05/01/LibGDX-Tutorial-11-Tiled-Maps-Part-2-Adding-a-character-sprite.aspx
+ * https://github.com/mrdoob/three.js/issues/69
  * */
 
-public class BattleCommander extends ApplicationAdapter {
+
+public class BattleCommander extends ApplicationAdapter implements InputProcessor {
 	
-	//Camera and map objects
-	private OrthographicCamera camera;
+	//Map and camera:
+	private TiledMap map;
+	private OrthographicCamera cam;
+	private TiledMapRenderer mapRenderer;
 	
-	private SpriteBatch batch;
-	private Rectangle testShape;
-	private Texture soldier;
-	private int frames=0,startTime=0,currentTime;
-	static final int WORLDX = 2000;
-	static final int WORLDY = 2000;
-	@SuppressWarnings("unused")
-	private float initialx,initialy;
-	private boolean flipx=false;
-	private float clickX=10,clickY=10;
+	//Sprites:
+	private SpriteBatch sb;
+	private Texture soldierTexture;
+	private Sprite soldier;
 	
 	
-	private Sprite mapSprite;
 	
 	@Override
 	public void create () {
-		System.out.println("Initializing game.");
 		
-		//MAP
-		mapSprite = new Sprite(new Texture(Gdx.files.internal("GreenGrid.png")));
-		mapSprite.setPosition(0, 0);
-		mapSprite.setSize(WORLDX, WORLDY);
+		//BATTLE MAP INITIALIZATION:
+		//Query width and height of window.
+		float w = Gdx.graphics.getWidth()/2;
+		float h = Gdx.graphics.getHeight()/2;
 		
-		//CAMERA AND WINDOW
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false,80,60);
+		//Initialize camera.
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false,w,h);
+		cam.update();
 		
-		//SPRITES AND OBJECTS
-		batch = new SpriteBatch();
-		soldier = new Texture(Gdx.files.internal("SoldierC.png"));
+		//Initialize map.
+		map = new TmxMapLoader().load("VirtMap1.tmx");
+		mapRenderer = new OrthogonalTiledMapRenderer(map);
 		
-
-		startTime = (int) (TimeUtils.millis()/1000);
+		Gdx.input.setInputProcessor(this);
 		
-		testShape = new Rectangle();
-		testShape.width = 64;
-		testShape.height = 64;
-		testShape.x = 8;
-		testShape.y = 0;
+		//SPRITE AND CHARACTER INITIALIZAITON:
+		//SpriteBatch
+		sb = new SpriteBatch();
 		
+		//Soldier:
+		soldierTexture = new Texture(Gdx.files.internal("SoldierC.png"));
+		soldier = new Sprite(soldierTexture);
 		
-		
-		
-		System.out.println("Loading assets.");
-		
-		
-
-		System.out.println("Game intialized.\n\n");
 	}
 
 	@Override
 	public void render () {
-		frames+=1;
-		//QUERY INPUT
-		queryInput();
-		
-		//CHECK VARIABLES
 		adjustVariables();
-		
-		//DRAW GRAPHICS
 		drawGraphics();
-
-		
-		
-		if(frames % 60 == 0){
-		currentTime = (int) (TimeUtils.millis()/1000 - startTime);
-		System.out.println(frames+" frames, ms:"+currentTime);}
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+
 
 	}
 	
 	@Override
 	public void resize(int width, int height){
-		//viewport.update(width,height);
+		cam.setToOrtho(false, width/2, height/2);
+		//update(width,height);
 	}
 	
-	public void queryInput(){
-		
-		//MOUSE INPUT
-		clickX = 80*Gdx.input.getX()/Gdx.graphics.getWidth();
-		clickY = 60-60*Gdx.input.getY()/Gdx.graphics.getHeight();
-		
-		//KEY INPUT
-		initialx = testShape.x;
-		initialy = testShape.y;
-		
-		if (Gdx.input.isKeyPressed(Keys.LEFT)){
-            System.out.println("Left");
-			testShape.x -= 1;}
-        if (Gdx.input.isKeyPressed(Keys.RIGHT)){
-        	System.out.println("Right");
-        	testShape.x += 1;}
-        if (Gdx.input.isKeyPressed(Keys.UP)){
-        	System.out.println("Up");
-        	testShape.y += 1;}
-        if (Gdx.input.isKeyPressed(Keys.DOWN)){
-        	System.out.println("Down");
-        	testShape.y -= 1;} 
-	}
 	public void adjustVariables(){
-		if(initialx>testShape.x){
-			flipx=true;
-		}else if(initialx<testShape.x){
-			flipx=false;
-		}
+		
 		
 	}
 	
 	public void drawGraphics(){
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		//SPRITEBATCH BEGIN
-		batch.begin();
-		batch.draw(soldier,clickX-16,clickY-16);
-		batch.draw(soldier,testShape.x, testShape.y, 32,32, 2, 0, 32, 32, flipx, false);
-		//batch.draw(soldier, testShape.x, testShape.y,flipx,false);
-		
-		//batch.draw(soldier, testShape.x+20, testShape.y+10,flipx,false);
-		
-		//SPRITEBATCH END
-		batch.end();
+		 Gdx.gl.glClearColor(0, 0, 0, 0);
+	     Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+	     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	     cam.update();
+	     mapRenderer.setView(cam);
+	     mapRenderer.render();
 	}
+	
+	@Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if(keycode == Input.Keys.LEFT)
+            cam.translate(-32,0);
+        if(keycode == Input.Keys.RIGHT)
+            cam.translate(32,0);
+        if(keycode == Input.Keys.UP)
+            cam.translate(0,32);
+        if(keycode == Input.Keys.DOWN)
+            cam.translate(0,-32);
+        if(keycode == Input.Keys.NUM_1)
+            map.getLayers().get(0).setVisible(!map.getLayers().get(0).isVisible());
+        if(keycode == Input.Keys.NUM_2)
+            map.getLayers().get(1).setVisible(!map.getLayers().get(1).isVisible());
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 	
 	
 }
