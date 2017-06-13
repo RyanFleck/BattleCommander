01648234534b574ea1df79df0f242acabd073bef
ryanfleck <ryan.fleck@protonmail.com>
2017-06-12 22:24:03 -0400

    REBUILD COMMIT. Using Stage2D. Replaced all calls to Gdx.graphics.get(width/height) with static vars.

--- core/src/com/libgdx/battlecommander/BattleCommander.java
+++ core/src/com/libgdx/battlecommander/BattleCommander.java
@@ -9,17 +9,30 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 
 /*
  * Resources used:
  * https://github.com/libgdx/libgdx/wiki/Orthographic-camera
  * http://www.gamefromscratch.com/post/2014/05/01/LibGDX-Tutorial-11-Tiled-Maps-Part-2-Adding-a-character-sprite.aspx
  * https://github.com/mrdoob/three.js/issues/69
+ * 
+ * Things to learn about:
+ * -Texture atlas and sprite-packers, for simple animation. 
  * */
 
 
 public class BattleCommander implements ApplicationListener{
 	
+	
+	//Window parameters:
+	private int winX,winY;
+	
+	//Frame monitoring:
+	private long frames;
+	private int fps;
+	private long time=0;
+	private String fString="",fpsString="";
 	
 	//HelloWorld objects:
 	private SpriteBatch sb;
@@ -47,12 +60,14 @@ public class BattleCommander implements ApplicationListener{
 		sb = new SpriteBatch();
 		text = new BitmapFont();
 		text.setColor(Color.CYAN);
+		winX=Gdx.graphics.getWidth();
+		winY=Gdx.graphics.getHeight();
 		
 		//BCOM upper-left logo:
 		BCOMLogo = new Texture(Gdx.files.internal("BComLogo.png"));
         BCOMSprite = new Sprite(BCOMLogo);
-        float logoX = (Gdx.graphics.getWidth()-BCOMSprite.getWidth());
-        float logoY = (Gdx.graphics.getHeight()-BCOMSprite.getHeight());
+        float logoX = (winX-BCOMSprite.getWidth());
+        float logoY = (winY-BCOMSprite.getHeight());
         BCOMSprite.setPosition(logoX,logoY);
 		
         //Dynamic texture test:
@@ -65,6 +80,8 @@ public class BattleCommander implements ApplicationListener{
         crosshairTex= new Texture(pixmap);
         crosshairSpr= new Sprite(crosshairTex);
         pixmap.dispose();
+        
+        
 		
 	}
 
@@ -79,8 +96,7 @@ public class BattleCommander implements ApplicationListener{
 	public void render() {
 		
 		//Set background color:
-		
-		Gdx.gl.glClearColor(0.1f,0.1f,0.1f,1);
+		Gdx.gl.glClearColor(0.15f,0.15f,0.15f,1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 		
         
@@ -89,15 +105,18 @@ public class BattleCommander implements ApplicationListener{
         
         
         //Draw dynamic texture:
-        crosshairSpr.setPosition((Gdx.graphics.getWidth()-150),10);
+        crosshairSpr.setPosition((winX-150),10);
         crosshairSpr.draw(sb);
         
         //Draw logo in upper right corner:
         BCOMSprite.draw(sb);
         
         //Draw version info:
+        monitorFrames();
+        text.draw(sb,fString,10,winY-10);
+        text.draw(sb,fpsString,10,winY-20);
         text.draw(sb,versionString,50,50);
-        text.draw(sb,"BATTLE COMMANDER",50,70);
+        text.draw(sb,"2017 Ryan Fleck",50,70);
         
         
         sb.end();//End sprite-batch creation...................................
@@ -106,7 +125,7 @@ public class BattleCommander implements ApplicationListener{
 
 	@Override
 	public void pause() {
-		
+	
 		
 	}
 
@@ -124,6 +143,17 @@ public class BattleCommander implements ApplicationListener{
 		
 	}
 
+	public void monitorFrames() {
+		frames++;
+		fps++;
+		fString=("Frame "+frames);
+		
+		if (TimeUtils.timeSinceNanos(time) > 1000000000) {
+			 time = TimeUtils.nanoTime();
+			 fpsString=("FPS: "+fps);
+			 fps=0;
+		}
+	}
 
 	
 	
